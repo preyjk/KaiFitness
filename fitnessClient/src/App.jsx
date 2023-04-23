fd9870b15635d9825cf4adcd4ce79ee9907c5b0c
it-tttt <1105974122@qq.com>
2023-04-23 15:43:00 +1200

    bug fix

--- fitnessClient/src/App.jsx
+++ fitnessClient/src/App.jsx
@@ -2,9 +2,9 @@
 import './App.css'
 import React from 'react'
 
-import Header from "./components/Header";
-import Home from './components/Home';
-import Footer from "./components/Footer";
+import Header from "./components/Header/Header";
+import Home from './components/Home/Home';
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
 
