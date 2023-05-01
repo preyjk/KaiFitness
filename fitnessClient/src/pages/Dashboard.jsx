600b677d3fff15109bea3701f4349197d9624db5
Winn0131 <110702691+Winn0131@users.noreply.github.com>
2023-05-01 13:08:45 +1200

    Add files via upload

---
+++ fitnessClient/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import Dashboard from '../components/Dashboard/Dashboard'
+
+export default function DashboardPage() {
+    return (
+        <div>
+            <Dashboard />
+        </div>
+    )
+    }
+    
