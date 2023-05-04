3f7cf1beca258c8b1dd4212389324b3b5c89f30c
Edmund <yzey474@aucklanduni.ac.nz>
2023-05-04 20:45:15 +1200

    fix try catch

--- fitnessServer/src/router/userRouter.js
+++ fitnessServer/src/router/userRouter.js
@@ -7,40 +7,53 @@ const sercetKey = require("../secret.js");
 const User = require("../db/user")
 const userRouter = express.Router()
 userRouter.post("/signUp",(req,res) =>{
-    const user = new User({
-        user : req.body.user,
-        password : md5(req.body.password),
-        gender: req.body.gender,
-        height: req.body.height,
-        weight: req.body.weight,
-        profilePicBase64: req.body.profilePicBase64
-    })
-    user.save().then(() =>{
-        res.status(200).send(user._id)
-    });
+    try{
+        const user = new User({
+            user : req.body.user,
+            password : md5(req.body.password),
+            gender: req.body.gender,
+            height: req.body.height,
+            weight: req.body.weight,
+            profilePicBase64: req.body.profilePicBase64
+        })
+        user.save().then(() =>{
+            res.status(200).send(user._id)
+        });
+    }
+    catch(e){
+        res.status(500).send(e)
+    }
 })
 
 
 userRouter.get("/signIn",async function(req,res){
-    let user = await User.findOne({user : req.query["user"]})
-    console.log(user)
-    let flag = md5(req.query["password"]) === user.password
-    if(flag){
-        let token = 'Bearer ' + jwt.sign(
-            {
-              _id: user._id,
-            },
-            sercetKey,
-            {
-              expiresIn: 3600 * 24 * 3
-            }
-          )
-          res.status(200).json({
-            status: 'ok',
-            data: { token: token }
-          })
-    }else{
-        res.status(400).json({"message":"password or username error!"})
+    try{
+        let user = await User.findOne({user : req.query["user"]})
+        console.log(user)
+        if(user === null){
+            res.status(400).json({"message":"password or username error!"})
+        }
+        let flag = md5(req.query["password"]) === user.password
+        if(flag){
+            let token = 'Bearer ' + jwt.sign(
+                {
+                _id: user._id,
+                },
+                sercetKey,
+                {
+                expiresIn: 3600 * 24 * 3
+                }
+            )
+            res.status(200).json({
+                status: 'ok',
+                data: { token: token }
+            })
+        }else{
+            res.status(400).json({"message":"password or username error!"})
+        }
+    }
+    catch(e){
+        res.status(500).send(e)
     }
 })
 
