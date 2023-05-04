46f5a3b902fa67043a97cd4a6221f0afa9d1f751
bzhu768 <127058350+bzhu768@users.noreply.github.com>
2023-05-04 13:41:46 +1200

    fix:login

--- fitnessClient/src/components/Header/index.jsx
+++ fitnessClient/src/components/Header/index.jsx
@@ -5,94 +5,111 @@
  * @LastEditTime: 2023-04-28 11:36:55
  * @Descripttion:
  */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Modal } from 'antd';
 import { UserOutlined, SettingOutlined } from '@ant-design/icons';
 import './index.less';
 import { Menu } from 'antd';
-
+import Login from '../Login';
 const items = [
-  {
-    label: 'WORKOUTS',
-    key: 'alipay',
-    icon: <SettingOutlined />,
-    children: [
-      {
-        label: 'WORKOUT VIDEOS',
-        key: 'setting:1'
-      },
-      {
-        label: 'CUSTOM WORKOUTS',
-        key: 'setting:2'
-      }
-    ]
-  },
-  {
-    label: 'PROGRAMS',
-    key: 'PROGRAMS',
-    children: [
-      {
-        label: 'WORKOUT PROGRAMS',
-        key: 'WORKOUT PROGRAMS'
-      },
-      {
-        label: 'MEAL PLANS',
-        key: 'PLANS'
-      }
-    ]
-  },
-  {
-    label: 'HEALTHY LIVING',
-    key: 'HEALTHY',
-    children: [
-      {
-        label: 'EXPERT ARTICLES',
-        key: 'EXPERT ARTICLES'
-      },
-      {
-        label: 'HEALTHY RECIPES',
-        key: 'HEALTHY RECIPES'
-      },
-      {
-        label: 'WELLNESS VIDEOS',
-        key: 'WELLNESS VIDEOS'
-      }
-    ]
-  },
-  {
-    label: 'ABOUT',
-    key: 'ABOUT',
-    children: [
-      {
-        label: 'ABOUT',
-        key: 'ARTICLES'
-      },
+	{
+		label: 'HOMEPAGE',
+		key: '/',
+		icon: <SettingOutlined />,
+	},
+	{
+		label: 'PLANSUBMITPAGE',
+		key: '/plan',
+	},
+	{
+		label: 'PERSONAL',
+		key: 'PERSONAL',
+		children: [
+			{
+				label: 'Dashboard',
+				key: '/Dashboard',
+			},
+			{
+				label: 'Add Plan',
+				key: 'Add Plan',
+			},
+		],
+	},
+	{
+		label: 'ABOUT',
+		key: 'ABOUT',
+		children: [
+			{
+				label: 'ABOUT',
+				key: 'ARTICLES',
+			},
 
-      {
-        label: 'OUR TEAM',
-        key: 'TEAM'
-      }
-    ]
-  }
+			{
+				label: 'OUR TEAM',
+				key: 'TEAM',
+			},
+		],
+	},
 ];
+const path = window.location.pathname;
+// const user = sessionStorage.getItem('user');
+const changeMenu = value => {
+	console.log(value);
+	window.location.href = value.key;
+};
 
-const user = sessionStorage.getItem('user');
-const App = () => (
-  <div className="header-box">
-    <div className="logo">
-      <a href="/"></a>
-    </div>
-    <Menu
-      className="menu-list"
-      style={{ flex: 'auto', minWidth: 0 }}
-      mode="horizontal"
-      items={items}
-    />
-    <div className="right">
-      <a className="tips">Hi,{user ? user : 'Sign in'}</a>
-      <UserOutlined className="user-icon" />
-    </div>
-    {/* <div style={sharedStyle} /> */}
-  </div>
-);
+const App = () => {
+	const [user, setuser] = useState('');
+	const [visible, setvisible] = useState(false);
+	const showLogin = () => {
+		setvisible(true);
+	};
+	const isModalOpen = () => {};
+	const close = value => {
+		setuser(value);
+		sessionStorage.setItem('username', value);
+		setvisible(false);
+	};
+	useEffect(() => {
+		let username = sessionStorage.getItem('username');
+		setuser(username);
+	}, []);
+
+	return (
+		<div className="header-box">
+			<div className="logo">
+				<a href="/"></a>
+			</div>
+			<Menu
+				onClick={changeMenu}
+				defaultSelectedKeys={path}
+				className="menu-list"
+				style={{ flex: 'auto', minWidth: 0 }}
+				mode="horizontal"
+				items={items}
+			/>
+			<div className="right">
+				<a
+					className="tips"
+					onClick={() => {
+						showLogin();
+					}}
+				>
+					Hi,{user ? user : 'Sign in'}
+				</a>
+				<UserOutlined className="user-icon" />
+			</div>
+			<Modal
+				onCancel={() => {
+					setvisible(false);
+				}}
+				visible={visible}
+				footer={null}
+			>
+				<Login close={close} />
+			</Modal>
+		</div>
+	);
+};
 
 export default App;
