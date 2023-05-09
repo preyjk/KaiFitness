74058656dd0805603ef12b72c4f155f95280bbff
it-tttt <41719927+it-tttt@users.noreply.github.com>
2023-05-09 19:44:18 +1200

    Add files via upload

--- fitnessClient/src/components/HomePage/Recipes/Recipes.jsx
+++ fitnessClient/src/components/HomePage/Recipes/Recipes.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { Button} from 'antd';
+import { StarTwoTone} from '@ant-design/icons';
 import './Recipes.css'
 
 export default function Recipes(props) {
     const {name, information, type, detail, muscleGroup, dietGroup} = props
+    const [starColor, setStarColor] = useState('gray');
+    const setStar=(e)=>{
+        // PubSub.publish('gettype',e.keyPath)
+        // setCurrent(e.key);
+        setStarColor(starColor=='gray'? 'orange': 'gray')
+        // console.log(e.target)
+        
+    }
     return (
         <section className="Recipes">
             <a href="#">
@@ -25,6 +35,10 @@ export default function Recipes(props) {
                     <p>{type=='diet'? `Meal Type:${dietGroup[0].diet}`: `Sport Type:${muscleGroup[0].muscle}`}</p>
                     <p>{type=='diet'? `Prep Time:${dietGroup[0].weight}`: `Group Number:${muscleGroup[0].number}`} </p>
                     <p>{type=='diet'? `Cook Time:${dietGroup[0]._id}`: `Weight:${muscleGroup[0].weight}`} </p>
+                    <Button onClick={setStar} className='star' type="text" shape="round" icon={<StarTwoTone twoToneColor={starColor}/>} size={'middle'}>
+                        
+                        Favorite
+                    </Button>
                 </div>
             </a>
         </section>
